7ee4552e038ab49964eefca0391eb5cebeeb8e5c
igor.bondartsov <bondartsov.igor@gmail.com>
2021-04-09 04:43:50 +0300

    Add my flow

---
+++ flows/flow.py
@@ -0,0 +1,20 @@
+from prefect import task, Flow
+from prefect.storage import GitHub
+
+@task
+def get_data():
+    return [1, 2, 3, 4, 5]
+
+@task
+def print_data(data):
+    print(data)
+
+with Flow("example") as flow:
+    data = get_data()
+    print_data(data)
+
+flow.storage = GitHub(
+    repo="IhorBondartsov/prefect-examples",   # name of repo
+    path="flows/flow.py",                    # location of flow file in repo
+    access_token_secret="GIT_TOKEN"   # name of personal access token secret
+)
